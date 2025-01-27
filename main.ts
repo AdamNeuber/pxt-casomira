2ee4de72ae6bf7273bc1f5545c63e799d3a58870
Adam Neuber <adam.neuber.024@pslib.cz>
2025-01-27 10:26:06 +0100

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,25 @@
 radio.setGroup(53)
-
+basic.pause(100)
 Sensors.SetLightLevel()
-input.onButtonPressed(Button.A, function() {
+
+input.onButtonPressed(Button.A, function () {
     Sensors.SetLightLevel()
 })
 
-Sensors.OnLightDrop(function() {
-    input.onButtonPressed(Button.AB, function() {
-        radio.sendNumber(0)
-    })
+Sensors.OnLightDrop(function () {
+    music.playTone(520, 400)
     radio.sendNumber(1)
 });
 
-radio.onReceivedNumber(function(receivedNumber: number) {
-    
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(0)
 })
 
+radio.onReceivedNumber(function (receivedNumber: number) {
+    music.playTone(440, 400)
+    basic.showNumber(receivedNumber)
+})
+
+input.onButtonPressed(Button.B, function() {
+    control.reset()
+})
